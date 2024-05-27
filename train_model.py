978cf4186a41b7b65f21b7d711b8c79f6287d842
ChelzeeMares <146792481+ChelzeeMares@users.noreply.github.com>
2024-05-27 02:02:04 -0500

    Update train_model.py
    Step 4: ML Pipeline instructions

--- train_model.py
+++ train_model.py
@@ -12,15 +12,16 @@ from ml.model import (
     save_model,
     train_model,
 )
-# TODO: load the cencus.csv data
+# Load census.csv data
+# DONE
 project_path = "Your path here"
 data_path = os.path.join(project_path, "data", "census.csv")
 print(data_path)
-data = # your code here
+data = pd.read_csv('census.csv')
 
-# TODO: split the provided data to have a train dataset and a test dataset
-# Optional enhancement, use K-fold cross validation instead of a train-test split.
-train, test = # Your code here
+# split into a training dataset and test dataset
+# DONE
+train, test = train_test_split(data, test_size=0.2, random_state=42)
 
 # DO NOT MODIFY
 cat_features = [
@@ -34,12 +35,10 @@ cat_features = [
     "native-country",
 ]
 
-# TODO: use the process_data function provided to process the data.
+# Use the process_data function provided to process the data.
+# DONE
 X_train, y_train, encoder, lb = process_data(
-    # your code here
-    # use the train dataset 
-    # use training=True
-    # do not need to pass encoder and lb as input
+    X_train, y_train, training=True
     )
 
 X_test, y_test, _, _ = process_data(
@@ -51,8 +50,9 @@ X_test, y_test, _, _ = process_data(
     lb=lb,
 )
 
-# TODO: use the train_model function to train the model on the training dataset
-model = # your code here
+# Use the train_model function to train the model on the training dataset
+# DONE
+model = train_model(X_train, y_train)
 
 # save the model and the encoder
 model_path = os.path.join(project_path, "model", "model.pkl")
@@ -65,22 +65,30 @@ model = load_model(
     model_path
 ) 
 
-# TODO: use the inference function to run the model inferences on the test dataset.
-preds = # your code here
+# Use the inference function to run the model inferences on the test dataset.
+# DONE
+preds = inference(model, X_test)
 
 # Calculate and print the metrics
 p, r, fb = compute_model_metrics(y_test, preds)
 print(f"Precision: {p:.4f} | Recall: {r:.4f} | F1: {fb:.4f}")
 
-# TODO: compute the performance on model slices using the performance_on_categorical_slice function
+# Compute the performance on model slices using the performance_on_categorical_slice function
 # iterate through the categorical features
+# DONE
 for col in cat_features:
     # iterate through the unique values in one categorical feature
     for slicevalue in sorted(test[col].unique()):
         count = test[test[col] == slicevalue].shape[0]
         p, r, fb = performance_on_categorical_slice(
-            # your code here
-            # use test, col and slicevalue as part of the input
+            data=test,
+            column_name=col,
+            slice_value=slicevalue
+            categorical_features=categorical_features,
+            label=label,
+            encoder=encoder,
+            lb=lb,
+            model=model
         )
         with open("slice_output.txt", "a") as f:
         print(f"{col}: {slicevalue}, Count: {count:,}", file=f)
