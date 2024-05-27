4f8bcd2f77b1690eefea8ee65fdc9945460c8c3c
ChelzeeMares <146792481+ChelzeeMares@users.noreply.github.com>
2024-05-27 03:34:57 -0500

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -21,7 +21,7 @@ data = pd.read_csv('census.csv')
 
 # split into a training dataset and test dataset
 # DONE
-X_train, X_test, y_train, y_test = train_test_split(data, test_size=0.2, random_state=42)
+train, test = train_test_split(data, test_size=0.2, random_state=42)
 
 # DO NOT MODIFY
 cat_features = [
