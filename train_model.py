b6e5337d0860730f737f57fc0e498834f66cfcd4
ChelzeeMares <146792481+ChelzeeMares@users.noreply.github.com>
2024-05-27 02:10:02 -0500

    Update train_model.py
    change project_path

--- train_model.py
+++ train_model.py
@@ -14,7 +14,7 @@ from ml.model import (
 )
 # Load census.csv data
 # DONE
-project_path = "Deploying-a-Scalable-ML-Pipeline-with-FastAPI/data/"
+project_path = "data"
 data_path = os.path.join(project_path, "data", "census.csv")
 print(data_path)
 data = pd.read_csv('census.csv')
