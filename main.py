638729e87ca5f21b99fe0ce5b6ea90407d8f524c
ChelzeeMares <146792481+ChelzeeMares@users.noreply.github.com>
2024-06-08 01:10:02 -0500

    Update main.py

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ async def get_root():
     pass
 
 
-# TODO: create a POST on a different path that does model inference
+# create a POST on a different path that does model inference
 @app.post("/data/")
 async def post_inference(data: Data):
     # DO NOT MODIFY: turn the Pydantic model into a dict.
