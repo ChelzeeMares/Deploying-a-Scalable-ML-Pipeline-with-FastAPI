c9226e72b1550946c0a57d16f087f4810b810ed4
ChelzeeMares <146792481+ChelzeeMares@users.noreply.github.com>
2024-06-08 01:19:26 -0500

    Update main.py

--- main.py
+++ main.py
@@ -26,10 +26,10 @@ class Data(BaseModel):
     hours_per_week: int = Field(..., example=40, alias="hours-per-week")
     native_country: str = Field(..., example="United-States", alias="native-country")
 
-path = 'model/encoder.pkl' 
+path = encoder_path
 encoder = load_model(path)
 
-path = 'model/model.pkl'
+path = model_path
 model = load_model(path)
 
 # create a RESTful API using FastAPI
