fbbf052070a0a6cdf4b84c181ab5b1ce0f51949a
ChelzeeMares <146792481+ChelzeeMares@users.noreply.github.com>
2024-06-17 00:59:15 -0500

    Update test_ml.py

--- test_ml.py
+++ test_ml.py
@@ -1,8 +1,6 @@
 import pytest
 import unittest
 from ml.model import train_model
-from ml.model import predict
-from ml.model import evaluate_model
 from sklearn.datasets import make_classification
 from sklearn.linear_model import LogisticRegression
 from ml.model import compute_model_metrics
