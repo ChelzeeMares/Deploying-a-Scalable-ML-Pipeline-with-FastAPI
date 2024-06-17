4aa0e8e80fda2288447bcb05f8be05a53eb1aad0
ChelzeeMares <146792481+ChelzeeMares@users.noreply.github.com>
2024-06-17 03:12:27 -0500

    Update model.py

--- ml/model.py
+++ ml/model.py
@@ -138,9 +138,9 @@ def performance_on_categorical_slice(
     """
 
     
-    slices = data[cat_features].unique()
+    slices = data[categorical_features].unique()
     for slice_value in slices:
-        data_slice = data[data[cat_features] == slice_value]
+        data_slice = data[data[categorical_features] == slice_value]
         X_slice, y_slice, _, _ = process_data(
             data_slice, 
             categorical_features=cat_features, 
