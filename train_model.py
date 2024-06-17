d5dd0622d4b9aba1324d7daaa317916c2b30c28e
ChelzeeMares <146792481+ChelzeeMares@users.noreply.github.com>
2024-06-17 05:03:46 -0500

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -104,7 +104,6 @@ for col in cat_features:
             encoder=encoder,
             lb=lb,
             model=model,
-            training=False
         )
         with open("slice_output.txt", "a") as f:
             print(f"{col}: {slicevalue}, Count: {count:,}", file=f)
