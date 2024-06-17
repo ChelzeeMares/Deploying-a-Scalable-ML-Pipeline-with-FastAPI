657201e8c28765cc463baa7712330035262ad0dc
ChelzeeMares <146792481+ChelzeeMares@users.noreply.github.com>
2024-06-17 02:18:02 -0500

    Update model.py

--- ml/model.py
+++ ml/model.py
@@ -136,6 +136,8 @@ def performance_on_categorical_slice(
     Done
 
     """
+
+    data = data[data[column_name] == slice_value]
     
     X_slice, y_slice, _, _ = process_data(
         data,
@@ -151,3 +153,6 @@ def performance_on_categorical_slice(
     preds = model.predict(X_slice)
     precision, recall, fbeta = compute_model_metrics(y_slice, preds)
     return precision, recall, fbeta
+
+
+
