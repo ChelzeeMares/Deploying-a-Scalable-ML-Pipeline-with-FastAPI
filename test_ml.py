ba0eff88dd8410473ce78596cdc0330e0fade454
ChelzeeMares <146792481+ChelzeeMares@users.noreply.github.com>
2024-06-17 00:34:27 -0500

    Update test_ml.py

--- test_ml.py
+++ test_ml.py
@@ -5,19 +5,23 @@ from sklearn.datasets import make_classification
 from sklearn.linear_model import LogisticRegression
 from ml.model import compute_model_metrics
 
-# TODO: add necessary import
-
-# TODO: implement the first test. Change the function name and input as needed
+# Test one
 def test_train_model(self):
     model = train_model()
     self.assertIsInstance(model, LogisticRegression)
 
-
-# TODO: implement the second test. Change the function name and input as needed
+# Test two
 def test_predict(self):
     predictions = predict()
     self.assertIsInstance(predictions, np.ndarray)
 
-def test_evaluate_model(self):
-    evaluation = evaluate_model()
-    self.assertIsInstance(evaluation, dict) 
+#Test three
+
+# Precision: 0.7285 | Recall: 0.2699 | F1: 0.3939
+
+def test_compute_model_metrics(self):
+    precision, recall, f1 = (0.7285, 0.2699, 0.3939)
+    
+    self.assertAlmostEqual(precision, self.expected_precision, places=4, msg="No match on Precision")
+    self.assertAlmostEqual(recall, self.expected_recall, places=4, msg="No match on recall")
+    self.assertAlmostEqual(f1, self.expected_f1, places=4, msg="No match on f1")
