3a13cb1e46f0cd0aef3db55cb8d9631103e9fa93
ChelzeeMares <146792481+ChelzeeMares@users.noreply.github.com>
2024-06-17 02:05:51 -0500

    Update model.py

--- ml/model.py
+++ ml/model.py
@@ -136,22 +136,12 @@ def performance_on_categorical_slice(
     Done
 
     """
-    cat_features = [
-    "workclass",
-    "education",
-    "marital-status",
-    "occupation",
-    "relationship",
-    "race",
-    "sex",
-    "native-country",
-]
     
     X_slice, y_slice, _, _ = process_data(
         data,
         column_name,
         slice_value,
-        categorical_features=cat_features,
+        categorical_features,
         label=label,
         encoder=encoder,
         lb=lb,
