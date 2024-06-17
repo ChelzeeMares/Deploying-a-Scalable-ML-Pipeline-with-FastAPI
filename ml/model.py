60b4821f66ba70e9c5c30c93ae08574a61926881
ChelzeeMares <146792481+ChelzeeMares@users.noreply.github.com>
2024-06-17 03:33:00 -0500

    Update model.py

--- ml/model.py
+++ ml/model.py
@@ -137,7 +137,7 @@ def performance_on_categorical_slice(
 
     """
 
-    """
+    
     slices = data[categorical_features].unique()
     for slice_value in slices:
         data_slice = data[data[categorical_features] == slice_value]
@@ -149,14 +149,7 @@ def performance_on_categorical_slice(
             encoder=encoder, 
             lb=lb
         )
-    """
-       # Ensure cat_feature is a single column name, not a list
-    slices = data[cat_feature].unique()
-    for slice_value in slices:
-        data_slice = data[data[cat_feature] == slice_value]
-        X_slice, y_slice, _, _ = process_data(
-            data_slice, categorical_features=categorical_features, label="salary", training=False, encoder=encoder, lb=lb
-        )
+   
         
       
 
