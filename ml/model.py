b6a083e4bbda4c49c843f412cc74b896575a8cf9
ChelzeeMares <146792481+ChelzeeMares@users.noreply.github.com>
2024-06-17 01:31:27 -0500

    Update model.py

--- ml/model.py
+++ ml/model.py
@@ -151,7 +151,6 @@ def performance_on_categorical_slice(
         "capital-loss",
         "hours-per-week",
         "native-country",
-        "salary",
     ]
     
     X_slice, y_slice, _, _ = process_data(
