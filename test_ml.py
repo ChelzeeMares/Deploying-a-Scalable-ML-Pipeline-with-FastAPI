25bf0ffdf2e76ba6dc4d40a1b41de02a70dc12ef
ChelzeeMares <146792481+ChelzeeMares@users.noreply.github.com>
2024-06-17 00:58:40 -0500

    Update test_ml.py

--- test_ml.py
+++ test_ml.py
@@ -1,6 +1,8 @@
 import pytest
 import unittest
 from ml.model import train_model
+from ml.model import predict
+from ml.model import evaluate_model
 from sklearn.datasets import make_classification
 from sklearn.linear_model import LogisticRegression
 from ml.model import compute_model_metrics
@@ -9,16 +11,8 @@ from ml.model import compute_model_metrics
 #Test train model
 def test_train_model():
     model = train_model()
-    assertIsInstance(model, LogisticRegression)
+    assert IsInstance(model, LogisticRegression)
 
-
-def test_predict():
-    predictions = predict()
-    assertIsInstance(predictions, np.ndarray)
-
-def test_evaluate_model():
-    evaluation = evaluate_model()
-    assertIsInstance(evaluation, dict) 
 
 
 
