868eed9f7ca7dbd8dab839f91825b34eeaea7f2e
ChelzeeMares <146792481+ChelzeeMares@users.noreply.github.com>
2024-06-17 03:48:18 -0500

    Update data.py

--- ml/data.py
+++ ml/data.py
@@ -43,7 +43,7 @@ def process_data(
         Trained LabelBinarizer if training is True, otherwise returns the binarizer
         passed in.
     """
-      if label is not None:
+    if label is not None:
         y = X[label]
         X = X.drop([label], axis=1)
     else:
