22054572d80373ccb90980dcc8ebb76461856f92
ChelzeeMares <146792481+ChelzeeMares@users.noreply.github.com>
2024-06-17 03:06:38 -0500

    Update model.py

--- ml/model.py
+++ ml/model.py
@@ -143,7 +143,7 @@ def performance_on_categorical_slice(
         data_slice = data[data[cat_feature] == slice_value]
         X_slice, y_slice, _, _ = process_data(
             data_slice, 
-            categorical_features=categorical_features, 
+            categorical_features=cat_features, 
             label="salary", 
             training=False, 
             encoder=encoder, 
